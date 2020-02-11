ce6635769131eba0c4cc0a6afbdf12446d1f4e65
unknown <alexirvin70@gmail.com>
2020-02-11 22:31:22 +0000

    added in death star api base service

---
+++ DeathstarApiService.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: owner
+ * Date: 11/02/2020
+ * Time: 17:25
+ */
+
+class DeathstarApiService
+{
+    private $curlClient;
+
+    private $baseUrl = 'http://deathstar.victoriaplum.com/alliance.php';
+
+    public function __construct()
+    {
+        $this->curlClient = curl_init();
+        curl_setopt($this->curlClient,CURLOPT_RETURNTRANSFER,1);
+    }
+
+
+    public function makeRequest($path)
+    {
+            $url = $this->buildUrl($path);
+
+            curl_setopt($this->curlClient, CURLOPT_URL, $url);
+
+            $result = curl_exec($this->curlClient);
+            $info = curl_getinfo($this->curlClient);
+
+            return [
+                'result' => json_decode($result, true),
+                'status_code' => $info['http_code'],
+            ];
+    }
+
+    private function buildUrl($path)
+    {
+        $url = $this->baseUrl . '?path=' . $path . '&name=test';
+        return $url;
+    }
+}
